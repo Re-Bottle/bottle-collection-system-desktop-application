394946964f74df8ca74c03f9063842c995885be0
Kenneth Fernandes <kennethdominic57@gmail.com>
2024-12-02 07:36:38 +0530

    added sample code to check interface with the GPIO pins

---
+++ interface/io_check.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO  # import RPi.GPIO module
+
+LED = 11  # pin no. as per BOARD, GPIO18 as per BCM
+
+GPIO.setwarnings(False)  # disable warnings
+GPIO.setmode(GPIO.BOARD)  # set pin numbering format
+GPIO.setup(LED, GPIO.OUT)  # set GPIO as output
+
+# Turn ON LED
+GPIO.output(LED, GPIO.HIGH)
