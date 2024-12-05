d435a0e5018a16b0706e375df90b52ce0889901a
Pearl-Dsilva <pearlveola.dsilva@mca.christuniversity.in>
2024-12-05 12:13:17 +0530

    fixed server code and added error handling

--- interface/server_communicate.py
+++ interface/server_communicate.py
@@ -1,13 +1,14 @@
 import requests
 
-SERVER = "http://localhost:8000"  # URL of the server
+SERVER = "http://localhost:3000"  # URL of the server
 
 
 def ping():
-    # Sending a GET request to the server
-    response = requests.get(SERVER)
+    try:
+        # Sending a GET request to the server
+        response = requests.get(SERVER)
 
-    if response.status_code == 200:
-        print("Ping was successful")
-    else:
-        print(f"Failed to ping the server, response code: {response.status_code}")
+        if response.status_code == 200:
+            print("Ping was successful")
+    except requests.exceptions.ConnectionError:
+        print("Failed to connect to the server")
